b835532615648740e2f53ee401cd725e6709b73c
Jeremy Buttice <jeremy@tymate.com>
2016-11-02 23:00:29 +0100

    Do not reproduce ! fix syntax error to migration file

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(version: 20161102214345) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
     t.string   "password_digest"
     t.string   "token"
-    t.jsonb    "settings",        default: "{}", null: false
+    t.jsonb    "settings",        default: {}, null: false
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
