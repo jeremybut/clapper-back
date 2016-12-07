53f7ebbd1f5e76e885d8ed33459ddd594953e55e
Jeremy Buttice <jeremy@tymate.com>
2016-12-07 16:14:19 +0100

    Add endpoint to Get Movie Details

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -8,6 +8,27 @@ module Api
         movies = kodi_client.request('VideoLibrary.GetMovies', 'libMovies')
         render json: movies, status: 200
       end
+
+      def show
+        movie = kodi_client.request(
+          'VideoLibrary.GetMovieDetails', 'Video.Fields.Movie', args
+        )
+        render json: movie, status: 200
+      end
+
+      private
+
+      def args
+        {
+          movieid: params[:id].to_i,
+          properties: %w(
+            title genre year rating director trailer mpaa top250 fanart votes
+            sorttitle resume setid dateadded tag art tagline plot plotoutline
+            originaltitle lastplayed playcount writer studio cast country
+            imdbnumber runtime set showlink streamdetails thumbnail file
+          )
+        }
+      end
     end
   end
 end
